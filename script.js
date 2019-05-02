723992b1a6eabe5f2e732a976983bb961fd870af
AnnaE <annaefitzgerald@gmail.com>
2019-05-02 16:30:33 -0700

    script.js

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ var generateCandidate = function(name, partyColor)
 }
 
 var alexaCortez = generateCandidate("Alexa Cortez", [132,17,11]);
-//console.log("Alexa's Color is " + alexaCortez.partyColor)
+console.log("Alexa's Color is " + alexaCortez.partyColor)
 var emyGonzalez = generateCandidate("Emy Gonzalez", [245,141,136]);
-//console.log("Emy's Color is " + emyGonzalez.partyColor)
+console.log("Emy's Color is " + emyGonzalez.partyColor)
 
   alexaCortez.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2, 2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
 
@@ -46,42 +46,31 @@ var setStateResults = function(state)
 var stateWinner = theStates[state].winner;
       if (stateWinner !== null) {
       theStates[state].rgbColor = stateWinner.partyColor;
- } else {
-   theStates[state].rgbColor = [11,32,57]
- }
+ } else {theStates[state].rgbColor = [11,32,57];}
 
-var stateInfoTable = document.getElementById('stateResults');
-var header = stateInfoTable.children[0].children[0];
-var stateName = header.children[0];
+ var stateInfoTable = document.getElementById('stateResults');
+ var header = stateInfoTable.children[0].children[0];
+ var stateName = header.children[0];
+ stateName.innerText = theStates[state].nameFull;
+ var stateAbbrev = header.children[1];
+ stateAbbrev.innerText = "(" + theStates[state].nameAbbrev + ")";
 
-  stateName.innerText = theStates[state].nameFull;
+ var body = stateInfoTable.children[1];
+ var candidate1Name = body.children[0].children[0];
+candidate1Name.innerText = alexaCortez.name;
 
-var stateAbbrev = header.children[1];
+ var candidate1StateResults = body.children[0].children[1];
+candidate1StateResults.innerText = alexaCortez.electionResults[state];
 
-  stateAbbrev.innerText = "(" + theStates[state].nameAbbrev + ")";
-
-var body = stateInfoTable.children[1];
-var candidate1Name = body.children[0].children[0];
-
-  candidate1Name.innerText = alexaCortez.name;
-
-var candidate1StateResults = body.children[0].children[1];
-
-  candidate1StateResults.innerText = alexaCortez.electionResults[state];
-
-var candidate2Name = body.children[1].children[0];
-
-  candidate2Name.innerText = emyGonzalez.name;
-
-var candidate2StateResults = body.children[1].children[1];
-
-  candidate2StateResults.innerText = emyGonzalez.electionResults[state];
+ var candidate2Name = body.children[1].children[0];
+ candidate2Name.innerText = emyGonzalez.name;
+ var candidate2StateResults = body.children[1].children[1];
+ candidate2StateResults.innerText = emyGonzalez.electionResults[state];
 
 var winnersName = body.children[2].children[1]
-winnersName.innerText = winner.name;
+winnersName.innerText = winner;
 
-if (theStates[state].winner === null) {
-  winnersName.innerText = "Re-vote";
+if (theStates[state].winner === null) {winnersName.innerText = "Re-vote";
 } else { winnersName.innerText = theStates[state].winner.name; }
 
 }
@@ -90,20 +79,19 @@ alexaCortez.tallyTotalVotes();
 emyGonzalez.tallyTotalVotes();
 
 
-var winner = null;
+ var winner = null;
 declareWinner = function()
 {
     if (alexaCortez.totalVotes > emyGonzalez.totalVotes)
-    {
-      winner = alexaCortez.name;
-    //console.log("Alexa Cortez is the Winner");}
+    {winner = alexaCortez.name;
+    console.log("Alexa Cortez is the Winner");}
 
     else if (emyGonzalez.totalVotes > alexaCortez.totalVotes)
-    {
-      winner = emyGonzalez.name;
-    //console.log("Emy Gonzalez is the Winner");
-  }  else  {
-      winner = "Re-Vote";}
+    {winner = emyGonzalez.name;
+    console.log("Emy Gonzalez is the Winner");}
+
+    else
+    {winner = "Re-Vote";}
 }
 
   declareWinner();
